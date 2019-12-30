c71d1ed8ab088f985716cec5f76772f2bd8ccc04
Dan Cazacu <dan.cazacu43@yahoo.ro>
2019-12-30 14:35:28 +0200

    Added static html and js login page

---
+++ web-app/main.js
@@ -0,0 +1,14 @@
+const home = document.getElementById('Home');
+const news = document.getElementById('News');
+const contact = document.getElementById('Contact');
+const about = document.getElementById('About');
+const logout = document.getElementById('Logout');
+
+function highlight(button) {
+    home.className = "inactive";
+    news.className = "inactive";
+    contact.className = "inactive";
+    about.className = "inactive";
+    logout.className = "inactive";
+    button.className = "active";
+}
