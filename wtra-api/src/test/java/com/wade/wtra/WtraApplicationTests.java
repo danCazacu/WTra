ac80a5fff530812200b95de6d09d6fa8ca3ab152
Dan Cazacu <dan.cazacu43@yahoo.ro>
2019-12-30 14:32:57 +0200

    Added first version of API with file upload

---
+++ wtra-api/src/test/java/com/wade/wtra/WtraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wade.wtra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
