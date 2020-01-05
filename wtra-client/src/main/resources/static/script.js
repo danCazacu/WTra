f9ddf733a6701a9780046ac6f3ac7cb26d36ce42
Dan Cazacu <dan.cazacu43@yahoo.ro>
2020-01-05 17:20:17 +0200

    fixed script.js

--- wtra-client/src/main/resources/static/script.js
+++ wtra-client/src/main/resources/static/script.js
@@ -3,24 +3,13 @@ const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 const proceedSignIn = document.getElementById('proceedSignIn');
 
-signUpButton.addEventListener('click', () = > {
+signUpButton.addEventListener('click', () => {
     container.classList.add("right-panel-active");
 })
 ;
 
-signInButton.addEventListener('click', () = > {
+signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 })
 ;
-
-proceedSignIn.addEventListener('click', () = > {
-    window.location = "main.html";
-return false;
-})
-;
-// function authorize(form) {
-// 	document.getElementById("signInEmail");
-// 	document.getElementById("signInPassword");
-// 	window.location.href = "main.html";
-// }
 
