c668e12ddd6dc2b36a97835b00639f14b8bcd703
Dan Cazacu <dan.cazacu43@yahoo.ro>
2020-01-28 22:18:43 +0200

    Refactored Your Uploads

---
+++ wtra-client/src/main/java/com/wtra/client/pojo/VideoPOJO.java
@@ -0,0 +1,28 @@
+package com.wtra.client.pojo;
+
+public class VideoPOJO {
+
+    String name;
+    String data;
+
+    public VideoPOJO(String name, String data) {
+        this.name = name;
+        this.data = data;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
